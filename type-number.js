3ed5b21e12586b78642f9e3b8f0851df543d4aca
ketlynkelly <127304211+ketlynkelly@users.noreply.github.com>
2023-09-13 08:18:36 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// tipo number
+
+const meunumero = 1;
+
+const primeironumero = 1;
+const segundonumero = 2;
+
+const operacaomatematica = primeironumero * segundonumero;
+
+console.log(operacaomatematica)
+
+// ponto flutuante
+
+const numeropontoflutuante = 3.3;
+const pontoflutuantesemzero = .5;
+
+const novaoperacao = primeironumero / numeropontoflutuante; 
+
+console.log(novaoperacao)
+
+//nan -> not a number (nao é um numero)
+
+const alura = "alura";
+console.log(alura * primeironumero) 
